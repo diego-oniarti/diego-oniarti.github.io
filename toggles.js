a48d2cce7372465a1ded569c629f9793ecc23c99
Diego <69463084+diego-oniarti@users.noreply.github.com>
2023-04-22 15:32:35 +0200

    fixati toggles

--- toggles.js
+++ toggles.js
@@ -1,9 +1,11 @@
 const toggles = [
     ()=>{
         document.getElementById('main').classList.toggle('dark');
+        document.getElementById('darkToggle').classList.toggle('on');
     },
     ()=>{
         document.getElementById('main').classList.toggle('en');
+        document.getElementById('languageToggle').classList.toggle('on');
     }
 ]
 
