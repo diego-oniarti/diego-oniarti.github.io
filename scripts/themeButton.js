71a82bb1a9e348af97e8ec0ffb5785d9b4731a27
Diego <69463084+diego-oniarti@users.noreply.github.com>
2023-10-12 21:58:22 +0200

    fix resize

--- scripts/themeButton.js
+++ scripts/themeButton.js
@@ -12,13 +12,19 @@ document.getElementById('navCollapse').addEventListener('click',e=>{
     }
 });
 
-for (let bottone of document.getElementsByClassName('collapseButton')) {
-    for (element of bottone.parentElement.parentElement.children) {
-        if (element.classList.contains('section')) {
+function resizeCollapsable () {
+    for (element of document.getElementsByClassName('section')) {
+        if (element.style.maxHeight!='0px')
             element.style.maxHeight = element.scrollHeight + "px";
-        }
     }
-    
+}
+resizeCollapsable();
+window.addEventListener('resize', ()=>{
+    console.log("A")
+    resizeCollapsable();
+})
+
+for (let bottone of document.getElementsByClassName('collapseButton')) {    
     bottone.addEventListener('click',e=>{
         bottone.children[0].classList.toggle('flipped')
         for (element of bottone.parentElement.parentElement.children) {
